3554882d003e4470d39ec52dd8949c6f984a97b3
Chris Jackson <chris.scott.jackson@gmail.com>
2022-01-22 00:27:27 -0800

    footer URL

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,49 +1,41 @@
 <script>
-  import Link from './Link.svelte'
+	import Link from './Link.svelte';
 
-  const currentYear = new Date().getFullYear()
+	const currentYear = new Date().getFullYear();
 </script>
 
-<style>
-  .wrapper {
-    position: fixed;
-    padding: 10px;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    background-color: var(--background-color);
-  }
-
-  .content {
-    width: clamp(20ch, 75%, 80ch);
-    margin: auto;
-  }
-
-  @media print {
-    .wrapper {
-      position: absolute;
-    }
-  }
-</style>
-
 <div class="wrapper">
-  <div class="content">
-    © Chris Jackson
-    {currentYear}
-    -
-    <Link
-      href="https://github.com/chrsjxn/chrsjxn-io"
-      external
-      externalIndicator={false}>
-      src
-    </Link>
-    -
-    <Link
-      href="https://twitter.com/c_jackson_js"
-      external
-      externalIndicator={false}>
-      twitter
-    </Link>
-    - Be kind!
-  </div>
+	<div class="content">
+		© Chris Jackson
+		{currentYear}
+		-
+		<Link href="https://github.com/chrsjxn/chrsjxn-io-ssr" external externalIndicator={false}>
+			src
+		</Link>
+		-
+		<Link href="https://twitter.com/c_jackson_js" external externalIndicator={false}>twitter</Link>
+		- Be kind!
+	</div>
 </div>
+
+<style>
+	.wrapper {
+		position: fixed;
+		padding: 10px;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		background-color: var(--background-color);
+	}
+
+	.content {
+		width: clamp(20ch, 75%, 80ch);
+		margin: auto;
+	}
+
+	@media print {
+		.wrapper {
+			position: absolute;
+		}
+	}
+</style>
