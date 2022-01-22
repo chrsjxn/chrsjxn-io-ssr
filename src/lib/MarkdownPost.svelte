b06c57786f13d1d29ce492474bc1375a878353d6
Chris Jackson <chris.scott.jackson@gmail.com>
2022-01-21 21:50:37 -0800

    fix syntax highlighting by configuring markdownit

--- src/lib/MarkdownPost.svelte
+++ src/lib/MarkdownPost.svelte
@@ -1,16 +1,5 @@
 <script>
-	import hljs from 'highlight.js/lib/core';
-	import bash from 'highlight.js/lib/languages/bash';
-	import javascript from 'highlight.js/lib/languages/javascript';
-	import css from 'highlight.js/lib/languages/css';
-	import hljsSvelte from 'highlightjs-svelte/dist/index.js';
 	import 'highlight.js/styles/a11y-light.css';
-
-	hljs.registerLanguage('bash', bash);
-	hljs.registerLanguage('javascript', javascript);
-	hljs.registerLanguage('css', css);
-
-	hljsSvelte(hljs);
 
 	export let markdown = '';
 </script>
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,39 @@
 import { Mode, plugin as markdown } from 'vite-plugin-markdown'
+import hljs from 'highlight.js/lib/core'
+import bash from 'highlight.js/lib/languages/bash'
+import javascript from 'highlight.js/lib/languages/javascript'
+import css from 'highlight.js/lib/languages/css'
+import hljsSvelte from 'highlightjs-svelte'
+import MarkdownIt from 'markdown-it'
+import Anchor from 'markdown-it-anchor'
+
+hljs.registerLanguage('bash', bash)
+hljs.registerLanguage('javascript', javascript)
+hljs.registerLanguage('css', css)
+
+hljsSvelte(hljs)
+
+const markdownIt = new MarkdownIt({
+	highlight: function (str, lang) {
+		if (lang && hljs.getLanguage(lang)) {
+			try {
+				return hljs.highlight(str, { language: lang }).value
+			} catch (e) {
+				// eslint-disable-next-line no-console
+				console.error('Failed to highlight string')
+			}
+		}
+
+		return '' // use external default escaping
+	},
+}).use(Anchor, {
+	slugify: (str) =>
+		str
+			.replace(/[^a-zA-Z-_ ]/g, '')
+			.toLowerCase()
+			.split(' ')
+			.join('-'),
+})
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +43,8 @@ const config = {
 		vite: {
 			plugins: [
 				markdown({
-					mode: Mode.HTML
+					mode: Mode.HTML,
+					markdownIt
 				})
 			]
 		}
